09e7dda4965ee991418968ed2a9ca2bdf8d34b28
danbanned <Djohn0082@launchpadphilly.org>
2025-12-27 23:15:30 -0800

    jhgfghj

--- app/communications/new/page.jsx
+++ app/communications/new/page.jsx
@@ -1,3 +1,6 @@
+export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
+
 import React from 'react'
 import CommunicationsNewClient from '../../components/communications/CommunicationsNewClient'
 
