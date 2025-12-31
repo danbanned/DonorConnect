1559e09f642df650ee79115b628701f4b9a62369
danbanned <Djohn0082@launchpadphilly.org>
2025-12-30 18:13:50 -0800

    jhgfdghj

--- app/api/auth/auditLog/route.js
+++ app/api/auth/auditLog/route.js
@@ -1,7 +1,7 @@
 export const runtime = 'nodejs'
 
 import { NextResponse } from 'next/server'
-import prisma from '@/lib/db'
+import prisma from '../../../../lib/db'
 
 export async function POST(request) {
   try {
